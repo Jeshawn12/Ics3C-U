f24dc51f663d66d2a491134ff55e14d78a70af83
JeshawnGodon <Jeshawn.BaptisteGordon@student.tdsb.on.ca>
2016-09-14 12:48:15 -0400

    Part 1

---
+++ Hello/src/gordon/AboutMe.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package gordon;
+
+/**
+ * @author 324003813
+ *
+ */
+public class AboutMe {
+
+	/**
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated constructor stub
+		System.out.println("Jeshawn Gordon");
+		System.out.println("Mr.K");
+		System.out.println("Runnymede CI");
+		System.out.println("\"Go Ravens Go!\"");
+	}
+
+}
