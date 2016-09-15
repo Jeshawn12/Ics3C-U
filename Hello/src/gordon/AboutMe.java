c6b7140e61b3f999bc0286a56e6428a4ebe2d461
JeshawnGodon <Jeshawn.BaptisteGordon@student.tdsb.on.ca>
2016-09-15 12:53:28 -0400

    Part 2

--- Hello/src/gordon/AboutMe.java
+++ Hello/src/gordon/AboutMe.java
@@ -11,6 +11,7 @@ public class AboutMe {
 
 	/**
 	 * 
+	 * 
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated constructor stub
@@ -18,6 +19,15 @@ public class AboutMe {
 		System.out.println("Mr.K");
 		System.out.println("Runnymede CI");
 		System.out.println("\"Go Ravens Go!\"");
+		
+		
+		
+		System.out.format("%-10s %8s %8s %15s %10s","Period","Class","Time","Teacher","Room\n \n");
+		System.out.format("%-10s %9s %9s %14s %9s","Period 1","ENG3C1-01","8:45-10:02","Mr.Gibbions","Room 101\n");
+		System.out.format("%-10s %9s %9s %11s %9s","Period 2","PAF3/4OM-01","10:07-11:24","Mr.Rowland","W.R.\n \n ");
+		System.out.format("%-10s %9s %9s %11s %9s","Lunch"," ","11:24-12:26"," "," \n \n ");
+		System.out.format("%-10s %9s %9s %11s %9s","Period 3","ICS3C/3U1-01","12:26-1:43","Ms.Kemp","Room 24\n");
+		System.out.format("%-10s %9s %9s %11s %9s","Period 4","SBI3C1-01","1:48-3:05","Mr.Crooks ","Room 208 \n");
 	}
 
 }
