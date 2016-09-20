a7e4534bbd51b9b6430815a2e3703a2ca6689a9d
JeshawnGodon <Jeshawn.BaptisteGordon@student.tdsb.on.ca>
2016-09-20 12:48:34 -0400

    TicTacToe Rules

---
+++ Hello/src/gordon/TicTacToeRules.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package gordon;
+
+/**
+ * @author 324003813
+ *
+ */
+public class TicTacToeRules {
+
+	/**TicTacToe.java
+	 * jeshawnGordon
+	 * 20/9/16
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("\n \t");
+		System.out.println("TicTacToe Rules \n \t");
+		System.out.println("Tic Tac Toe is a game for 2 players.\n");
+		System.out.println("1. A grid of 9 squares is drawn.\n");
+		System.out.println("2. The first player draws an X in an empty square.\n");
+		System.out.println("3. The second player draws an O in an empty square.\n");
+		System.out.println("4. Steps 2 and 3 are repeated until a Tic Tac pattern is created:\n \n pattern can be diagonal, horizonal, or vertical.\n");
+		System.out.println("5. A winning pattern is completed by drawing a line throught th pattern.");
+
+	}
+
+}
