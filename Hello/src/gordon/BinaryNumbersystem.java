1efef7d8fea1c3f99f6042979383f06bc49fa0be
JeshawnGodon <Jeshawn.BaptisteGordon@student.tdsb.on.ca>
2016-09-20 13:31:06 -0400

    BinaryNumberSystem

---
+++ Hello/src/gordon/BinaryNumbersystem.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package gordon;
+
+/**
+ * @author 324003813
+ *
+ */
+public class BinaryNumbersystem {
+
+	/**BinaryNumbersystem.java
+	 * jeshawnGordon
+	 * 20/9/16
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("\n");
+		System.out.format("%15s %20s"," Decimal","Binary \n");
+		System.out.format("%15s %20s","1","1 \n");
+		System.out.format("%15s %20s","2","10 \n");
+		System.out.format("%15s %20s","3","11 \n");
+		System.out.format("%15s %20s","4","100 \n");
+		System.out.format("%15s %20s","5","101 \n");
+		System.out.format("%15s %20s","6","110 \n");
+		System.out.format("%15s %20s","7","111 \n");
+		System.out.format("%15s %20s","8","1000 \n");
+		System.out.format("%15s %20s","9","1001 \n");
+		System.out.format("%15s %20s","10","1010 \n");
+		System.out.format("%15s %20s","11","1011 \n");
+		System.out.format("%15s %20s","12","1100 \n");
+		System.out.format("%15s %20s","13","1101 \n");
+		System.out.format("%15s %20s","14","1110 \n");
+		System.out.format("%15s %20s","15","1111 \n");
+		System.out.format("%15s %20s","16","10000 \n");
+		System.out.format("%15s %20s","17","10001 \n");
+		System.out.format("%15s %20s","18","10010 \n");
+		System.out.format("%15s %20s","19","10011 \n");
+		System.out.format("%15s %20s","20","10100 \n");
+	}
+
+}
