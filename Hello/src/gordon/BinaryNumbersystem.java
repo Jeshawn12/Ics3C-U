99922774dfe38d3875ee636202eedb31b84da754
JeshawnGodon <Jeshawn.BaptisteGordon@student.tdsb.on.ca>
2016-09-20 13:39:54 -0400

    BinaryNumberSystem Part B

--- Hello/src/gordon/BinaryNumbersystem.java
+++ Hello/src/gordon/BinaryNumbersystem.java
@@ -16,27 +16,27 @@ public class BinaryNumbersystem {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("\n");
-		System.out.format("%15s %20s"," Decimal","Binary \n");
-		System.out.format("%15s %20s","1","1 \n");
-		System.out.format("%15s %20s","2","10 \n");
-		System.out.format("%15s %20s","3","11 \n");
-		System.out.format("%15s %20s","4","100 \n");
-		System.out.format("%15s %20s","5","101 \n");
-		System.out.format("%15s %20s","6","110 \n");
-		System.out.format("%15s %20s","7","111 \n");
-		System.out.format("%15s %20s","8","1000 \n");
-		System.out.format("%15s %20s","9","1001 \n");
-		System.out.format("%15s %20s","10","1010 \n");
-		System.out.format("%15s %20s","11","1011 \n");
-		System.out.format("%15s %20s","12","1100 \n");
-		System.out.format("%15s %20s","13","1101 \n");
-		System.out.format("%15s %20s","14","1110 \n");
-		System.out.format("%15s %20s","15","1111 \n");
-		System.out.format("%15s %20s","16","10000 \n");
-		System.out.format("%15s %20s","17","10001 \n");
-		System.out.format("%15s %20s","18","10010 \n");
-		System.out.format("%15s %20s","19","10011 \n");
-		System.out.format("%15s %20s","20","10100 \n");
+		System.out.format("%15s %20s %20s"," Decimal","Binary","Hexadecimal \n");
+		System.out.format("%15s %20s %20s","1","1","1 \n");
+		System.out.format("%15s %20s %20s","2","10","2 \n");
+		System.out.format("%15s %20s %20s","3","11","3 \n");
+		System.out.format("%15s %20s %20s","4","100","4 \n");
+		System.out.format("%15s %20s %20s","5","101","5 \n");
+		System.out.format("%15s %20s %20s","6","110","6 \n");
+		System.out.format("%15s %20s %20s","7","111","7 \n");
+		System.out.format("%15s %20s %20s","8","1000","8 \n");
+		System.out.format("%15s %20s %20s","9","1001","9 \n");
+		System.out.format("%15s %20s %20s","10","1010","A \n");
+		System.out.format("%15s %20s %20s","11","1011","B \n");
+		System.out.format("%15s %20s %20s","12","1100","C \n");
+		System.out.format("%15s %20s %20s","13","1101","D \n");
+		System.out.format("%15s %20s %20s","14","1110","E \n");
+		System.out.format("%15s %20s %20s","15","1111","F \n");
+		System.out.format("%15s %20s %20s","16","10000","10 \n");
+		System.out.format("%15s %20s %20s","17","10001","11 \n");
+		System.out.format("%15s %20s %20s","18","10010","12 \n");
+		System.out.format("%15s %20s %20s","19","10011","13 \n");
+		System.out.format("%15s %20s %20s","20","10100","14 \n");
 	}
 
 }
