31d52b1650b01325c65ec6c9cc11c0876aaa77af
JeshawnGodon <Jeshawn.BaptisteGordon@student.tdsb.on.ca>
2017-01-12 13:14:29 -0500

    MISSISSIPPI file

---
+++ Unit 3/src/gordon/Mississippi.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package gordon;
+
+/**
+ * Jeshawn
+ * 12/02/17
+ * spelling out MISSISSIPPI using block letters
+ *MISSISSIPPI
+ */
+public class Mississippi {
+	public static void letterM(){
+		System.out.println("  M        M");
+		System.out.println("  MM      MM");
+		System.out.println("  M  M  M  M");
+		System.out.println("  M    M   M");
+		System.out.println("  M        M");
+		System.out.println("  M        M");
+		System.out.println("  M        M");
+
+	}
+	
+	public static void letterI(){
+		System.out.println("     IIIII");
+		System.out.println("       I");
+		System.out.println("       I");
+		System.out.println("       I");
+		System.out.println("       I");
+		System.out.println("       I");
+		System.out.println("     IIIII");
+	}
+	
+	public static void letterS(){
+		System.out.println("     SSSSS");
+		System.out.println("    S     S");
+		System.out.println("    S");
+		System.out.println("     SSSSS");
+		System.out.println("          S");
+		System.out.println("    S     S");
+		System.out.println("     SSSSS");
+	}
+	
+	public static void letterP(){
+		System.out.println("   PPPPPPPPP");
+		System.out.println("   PP      PP");
+		System.out.println("   PP      PP");
+		System.out.println("   PP      PP");
+		System.out.println("   PPPPPPPP");
+		System.out.println("   PP");
+		System.out.println("   PP");
+		System.out.println("   PP");
+		
+		
+	}
+
+	
+
+	
+	
+	/**
+	 * @param args
+	 */		
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		letterM();
+		letterI();
+		letterS();
+		letterS();
+		letterI();
+		letterS();
+		letterS();
+		letterI();
+		letterP();
+		letterP();
+		letterI();
+	}
+
+}
